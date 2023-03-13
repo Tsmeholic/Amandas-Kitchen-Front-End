fe4ea5a98e0fe0fe17e073f3480b5750395f5a71
Tsmeholic <tsmeholic@gmail.com>
2023-03-12 21:07:20 -0400

    Added firebase auto deploy when update pushed to github.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, RouterProvider } from 'react-router-dom';
 import { router } from "./router";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuSHG_aGRqI-w6z6NeLAP2i1NBVEOCmb0",
+  authDomain: "amandas-kitchen-front-en-d2db8.firebaseapp.com",
+  projectId: "amandas-kitchen-front-en-d2db8",
+  storageBucket: "amandas-kitchen-front-en-d2db8.appspot.com",
+  messagingSenderId: "684971781241",
+  appId: "1:684971781241:web:206d1a96db07a7f2e038a8",
+  measurementId: "G-NJFR3JKN07"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
